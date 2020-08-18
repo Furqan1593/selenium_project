f55c0d2b9eeac13c70dc6240a2b87dd2948e2266
furqan <m.furqan1593@gmail.com>
2020-08-18 15:11:38 +0500

    print option

--- github12/src/github12/test.java
+++ github12/src/github12/test.java
@@ -11,6 +11,10 @@ public class test {
     System.out.println("furqan ");
     System.out.println("furqan ");
     System.out.println("furqan ");
+    System.out.println("furqan ");
+    System.out.println("furqan ");
+    System.out.println("furqan ");
+    System.out.println("furqan ");
   }
 /*>>>>>>> f76cedd71b1b55a876c7cdb28ddda7ac235083c2*/
 }
